28c41486c4d20e85c4ac239c35396b9131210600
Pedro Nadolny <pedro.nadolny@gmail.com>
2024-02-29 22:15:02 -0300

    jumping

--- PlayerController.gd
+++ PlayerController.gd
@@ -1,10 +1,45 @@
 extends CharacterBody2D
 
+const gravity = 1500
+const max_horizontal_speed = 300
+const max_horizontal_acc = 1000
+const horizontal_dumping = 300
+const min_speed = 10
+const jump_force = 550
+
+var max_jumps = 2
+var jumps_available = 0
 var is_dashing: bool = false
-var gravity = 700
 
 func _physics_process(delta):
-	if not is_on_floor() and not is_dashing:
-		velocity.y += gravity * delta
+	apply_gravity(delta)
+	horizontal_input(delta)
+	jump_input(delta)
 		
 	move_and_slide()
+
+func apply_gravity(delta):
+	if is_on_floor() or is_dashing:
+		return
+	
+	velocity.y += gravity * delta
+		
+func horizontal_input(delta):
+	var input = Input.get_axis("move_left", "move_right")
+	var acc = input * max_horizontal_acc
+	velocity.x += acc * delta
+	velocity.x = lerp(velocity.x, 0.0, pow(0.5, horizontal_dumping * delta))
+	velocity.x = clampf(velocity.x , -max_horizontal_speed, max_horizontal_speed)
+	
+	if abs(velocity.x) < 5:
+		velocity.x = 0
+	
+func jump_input(delta):
+	if is_on_floor():
+		jumps_available = max_jumps
+	
+	if not Input.is_action_just_pressed("jump") or jumps_available < 1:
+		return
+		
+	jumps_available -= 1
+	velocity.y = -jump_force 

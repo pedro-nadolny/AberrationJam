b7a7bf14f7170f5d3aede8089e95fa7075cba576
Pedro Nadolny <pedro.nadolny@gmail.com>
2024-02-29 22:35:15 -0300

    dump horizontal movement only on floor

--- PlayerController.gd
+++ PlayerController.gd
@@ -9,7 +9,10 @@ const jump_force = 550
 
 var max_jumps = 2
 var jumps_available = 0
+
 var is_dashing: bool = false
+var max_dashes = 1
+var dashes_available = 0
 
 func _physics_process(delta):
 	apply_gravity(delta)
@@ -28,7 +31,10 @@ func horizontal_input(delta):
 	var input = Input.get_axis("move_left", "move_right")
 	var acc = input * max_horizontal_acc
 	velocity.x += acc * delta
-	velocity.x = lerp(velocity.x, 0.0, pow(0.5, horizontal_dumping * delta))
+	
+	if is_on_floor():
+		velocity.x = lerp(velocity.x, 0.0, pow(0.5, horizontal_dumping * delta))
+		
 	velocity.x = clampf(velocity.x , -max_horizontal_speed, max_horizontal_speed)
 	
 	if abs(velocity.x) < 5:
@@ -42,4 +48,11 @@ func jump_input(delta):
 		return
 		
 	jumps_available -= 1
-	velocity.y = -jump_force 
+	velocity.y = -jump_force
+	
+func dash_input(delta):
+	if dashes_available == 0:
+		return
+		
+	velocity.x += signf(velocity.x)
+	

3304982fd57a2c75298093b411b07b743d06a462
Pedro Nadolny <pedro.nadolny@gmail.com>
2024-03-05 19:03:15 -0300

    checkpoint system

--- CheckpointManager.gd
+++ CheckpointManager.gd
@@ -1,1 +1,11 @@
 extends Node2D
+
+var last_checkpoint = 0
+
+func checkpoint_entered(checkpoint):
+	var checkpoint_id = get_children().find(checkpoint)	
+	
+	if last_checkpoint > checkpoint_id:
+		return
+		
+	%Spawn.position = checkpoint.position
---
+++ entities/checkpoint.gd
@@ -0,0 +1,8 @@
+extends Area2D
+
+func _on_checkpoint_body_entered(body):
+	if body != %Player:
+		return
+		
+	%CheckpointManager.checkpoint_entered(self)
+	queue_free()
